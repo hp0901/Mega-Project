659464bdbc24a156bbd1f94f2854e8b5d3b5e2a2
hp0901 <hp98570018@gmail.com>
2025-10-06 16:17:56 +0530

    fix: replaced contactus try to solve Error

--- src/services/apis.js
+++ src/services/apis.js
@@ -64,7 +64,7 @@ export const catalogData = {
 
 // CONTACT-US API
 export const contactusEndpoint = {
-    CONTACT_US_API: BASE_URL + "/reach/contact",
+    CONTACT_US_API: BASE_URL + "/api/v1/reach/contact",
 };
 
 // SETTINGS PAGE API
