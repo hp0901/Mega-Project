3a5aaac9223e5b1bc639cc20f783eecb3d6ee8f6
hp0901 <hp98570018@gmail.com>
2025-10-06 18:30:18 +0530

    update contact from

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
+const BASE_URL = "https://mega-project-bs9q.onrender.com" || "http://localhost:4000";
 
 // AUTH ENDPOINTS
 export const endpoints = { // Changed to authEndpoints for clarity
