04ce2dad5c1ab6eb05ca6f46b67ab24fb1499a31
hp0901 <hp98570018@gmail.com>
2025-10-23 09:09:59 +0530

    feat: Update API URL to use secure HTTPS endpoint

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,7 +2,7 @@
 // const BASE_URL = "https://mega-project-bs9q.onrender.com/api/v1";
 
 // const BASE_URL = "http://localhost:4000/api/v1";
-const BASE_URL = "http://65.0.135.56:4000/api/v1";
+const BASE_URL = "https://harsh.myftp.org/api/v1";
 
 // ======================== AUTH ENDPOINTS ========================
 export const endpoints = {
